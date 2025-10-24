5af257c76c5c6b170fd6fc63bdbf2857961c8be6
hancheol.cho <chcbaram@gmail.com>
2025-10-24 15:27:31 +0900

    초기 프로젝트 추가

---
+++ firmware/stm32h5-fw/src/hw/hw.h
@@ -0,0 +1,33 @@
+#ifndef HW_H_
+#define HW_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "hw_def.h"
+
+
+#include "led.h"
+#include "uart.h"
+#include "cli.h"
+#include "log.h"
+#include "fault.h"
+#include "swtimer.h"
+#include "reset.h"
+#include "rtc.h"
+#include "qbuffer.h"
+#include "usb.h"
+#include "cdc.h"
+
+#include "osal/thread.h"
+
+
+bool hwInit(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
